8f80332ba19263b2bd5aabe45a50d9debb8285ca
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2023-05-07 12:57:41 +0100

    feat(api): add GET /health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    run(bind_port())?.await
+}
+
+fn bind_port() -> TcpListener {
+    return TcpListener::bind("127.0.0.1:8080").expect("Failed to bind random port");
 }
